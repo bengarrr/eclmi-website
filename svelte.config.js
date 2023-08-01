7c4560f6aa123a0e6db2a1ba8b9efaaf04b49c46
bengarrr <kevindwaite@gmail.com>
2023-08-01 02:43:55 -0700

    init

--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'app.html'
+		}),
 		alias: {
 			'@storyblok/svelte': './node_modules/@storyblok/svelte'
-		}
+		},
 	},
 	preprocess: vitePreprocess()
 };
