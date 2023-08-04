a3bcd0961394a9bec1d9f209fb4cdad741c3c8c0
bengarrr <kevindwaite@gmail.com>
2023-08-04 00:33:27 -0700

    mobile nav

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -11,7 +11,7 @@
 <!-- Desktop Navigation -->
 <div class="desktop-nav hidden fixed top-0 w-full z-10 lg:flex flex-row h-[100px] bg-[#11743b] px-[40px] items-center text-[#f4f4f5]">
     <div class="logo-wrapper">
-        <img src="/assets/logo.png" width="" height="" alt=""/>
+        <img src="/assets/logo.png" alt="Environmental Concepts logo"/>
     </div>
     <div class="nav-links flex flex-row ml-auto gap-[45px] xl:gap-[115px]">
         <a href="/" class="uppercase">Home</a>
@@ -24,7 +24,7 @@
 <!--Mobile Navigation-->
 
 <div class="mobile-nav flex flex-row lg:hidden fixed top-0 w-full z-10 h-[60px] bg-[#11743b] text-zinc-100">
-    <button class="menu-button hover:cursor-pointer" on:click={toggleDrawer}>
+    <button class="menu-button flex flex-row items-center justify-center hover:cursor-pointer" on:click={toggleDrawer}>
         <div class="hamburger-icon {drawerOpen?"hidden":""}">
             <img src="/assets/menu-button.svg" />
         </div>
@@ -39,7 +39,7 @@
             <a href="/services" class="uppercase w-full py-[20px] text-right hover:bg-[#0b4a25]">Services</a>
             <a href="/contact" class="uppercase w-full py-[20px] text-right hover:bg-[#0b4a25]">Contact Us</a>
             <div class="self-end w-full h-auto">
-                <img class="w-full h-full object-contain" src="/assets/logo.png" />
+                <img class="w-full h-full object-contain" src="/assets/logo.png" alt="Environmental Concepts logo" />
             </div>
         </div>
     </div>
