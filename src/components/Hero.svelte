f081facba85f9e3131fd9809598ed3293396e5de
bengarrr <kevindwaite@gmail.com>
2023-08-06 21:47:46 -0700

    hero image translation fix, carousel image slider fix, carousel modal image fix

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -9,22 +9,27 @@
         height: 1080
     }
 
+    const transitionTime = 1000;
+
     let index = 0;
 
     function transition(timeout) {
         const prev = index;
         index = (index + 1) % images.length;
-        _images.children[prev].classList.remove("hero-current")
+        _images.children[prev].classList.remove("hero-current");
         _images.children[prev].classList.add("hidden");
-        _images.children[index].classList.add("hero-current");
+        
         _images.children[index].classList.remove("hidden");
+        const reflow = _images.children[index].offsetHeight;
+
+        _images.children[index].classList.add("hero-current");
 
         setTimeout(transition, timeout, timeout)
     }
 
     onMount(() => {
         if(images.length > 1) {
-            setTimeout(transition, 6000, 5200);
+            setTimeout(transition, 6000, 5200+[transitionTime]);
         }
     })
 </script>
@@ -34,7 +39,7 @@
         {#if images.length>0}
         {#each images as { filename, alt }, i }
             {#if i === 0}
-                <div class="hero-image hero-current transition-all duration-700 ease-in-out [transform:translate(100%,0)] w-[100vw] h-[calc(100vh-100px)]">
+                <div class="hero-image hero-current transition-all duration-[{transitionTime}ms] ease-in-out w-[100vw] h-[calc(100vh-100px)]">
                     <img
                         class="w-full h-full object-cover" 
                         src="{filename}" 
@@ -42,7 +47,7 @@
                     />
                 </div>
             {:else}
-                <div class="hero-image hidden transition-all duration-700 ease-in-out [transform:translate(100%,0)] w-[100vw] h-[calc(100vh-100px)]">
+                <div class="hero-image hidden transition-all duration-[{transitionTime}ms] ease-in-out w-[100vw] h-[calc(100vh-100px)]">
                     <img
                         class="w-full h-full object-cover"  
                         src="{filename}"
@@ -54,9 +59,3 @@
         {/if}
     </div>
 </div>
-
-<style>
-    .hero-current {
-        transform: translate(0,0) !important;
-    }
-</style>
--- src/components/ServiceSection.svelte
+++ src/components/ServiceSection.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { onMount } from "svelte";
     import { storyblokEditable, renderRichText } from "@storyblok/svelte";
 	import ImageGrid from "./ImageGrid.svelte";
 
@@ -15,6 +16,17 @@
 
     const carousel_tranform_amount = 547;
     const carousel_tranform_amount_mobile = 250;
+    const carousel_gap_amount = 16;
+
+    function getPositionRight(element) {
+        const {top, right, width, height} = element.getBoundingClientRect();
+        return {
+            x: right,
+            y: top,
+            width,
+            height
+        };
+    }
 
     function toggleServices() {
         if(!service_dropdown.classList.contains("h-auto")) {
@@ -29,35 +41,54 @@
         if(modal.classList.contains("hidden")) {
             let image = document.createElement("img");
             image.src = image_path;
-            image.classList.add(["w-full", "h-full", "object-contain"]);
+            image.classList.add("w-full", "h-full", "object-contain");
             modal_image.appendChild(image);
             modal.classList.remove("hidden");
+            document.body.style.overflow = "hidden";
         } else if (!modal.classList.contains("hidden")) {
-            modal_image.removeChild(modal_image.children()[0]);
+            modal_image.removeChild(modal_image.children[0]);
             modal.classList.add("hidden");
+            document.body.style.overflow = "auto";
         }
     }
 
     function carouselLeft() {
         if(carousel_index !== 0) {
             carousel_index -= 1;
+            const style = window.getComputedStyle(carousel);
+            const currTranslation = new WebKitCSSMatrix(style.transform);
+            const currX = Math.abs(currTranslation.m41);
 
-            if (breakpoint.matches) {
-                carousel.storyblokEditable.transform = `translate(-${carousel_tranform_amount_mobile*carousel_index},0)`
-            }
-            carousel.storyblokEditable.transform = `translate(-${carousel_tranform_amount*carousel_index},0)`
+            const { width } = getPositionRight(carousel.children[carousel_index]);
+
+            carousel.style.transform = `translate(-${currX-(width+32)}px, 0px)`;
+        } 
+        if(carousel_index === 0) {
+            carousel.style.transform = `translate(0px, 0px)`;
         }
     }
 
     function carouselRight() {
         if(carousel_index < blok.gallery.length-1) {
+            const style = window.getComputedStyle(carousel);
+            const currTranslation = new WebKitCSSMatrix(style.transform);
+            const currX = Math.abs(currTranslation.m41);
+            const { x } = getPositionRight(carousel.children[carousel_index]);
+
+            // if (breakpoint.matches) {
+            //     carousel.style.transform = `translate(-${carousel_tranform_amount_mobile*carousel_index+(carousel_gap_amount*carousel_index)}px,0px)`;
+            // } else {
+            //     carousel.style.transform = `translate(-${carousel_tranform_amount*carousel_index+(carousel_gap_amount*carousel_index)}px,0px)`;
+            // }
+            carousel.style.transform = `translate(-${x+currX}px,0px)`;
+
             carousel_index += 1;
-            if (breakpoint.matches) {
-                carousel.storyblokEditable.transform = `translate(${carousel_tranform_amount_mobile*carousel_index},0)`
-            }
-            carousel.storyblokEditable.transform = `translate(${carousel_tranform_amount*carousel_index},0)`
         }
     }
+
+    onMount(() => {
+        breakpoint = window.matchMedia(breakpoint);
+    })
 
     $: description = renderRichText(blok.description);
     $: services = renderRichText(blok.services);
@@ -89,7 +120,7 @@
     </div>
 </div>
 <div class="image-carousel relative overflow-hidden w-[100vw]">
-    <div class="image-carousel--images flex flex-row pl-[10px] lg:pl-[40px] py-[40px] gap-8">
+    <div class="image-carousel--images flex flex-row pl-[10px] lg:pl-[40px] py-[40px] gap-8 transition-all duration-700 ease-in-out" bind:this={carousel}>
         {#if blok.gallery.length>0 }
         {#each blok.gallery as image, i}
             <div class="rounded-[20px] overflow-hidden basis-[250px] lg:basis-[547px] shrink-0 h-auto hover:cursor-pointer" role="button" tabindex=0 on:keydown={(event) => { event.key === "Enter" ? toggleGalleryModal(image.filename) : null }} on:click={() => { toggleGalleryModal(image.filename) }}>
@@ -98,21 +129,27 @@
         {/each}
         {/if}
     </div>
-    <div class="carousel-controls absolute bottom-[50%] [transform:translate(0,50%)] flex flex-row items-center w-[100vw] px-[15px] lg:px-[55px]">
-        <button class="left-arrow w-[30px] h-[30px] lg:w-[80px] lg:h-[80px] hover:cursor-pointer" on:click={carouselLeft}>
+    <div class="carousel-controls pointer-events-none absolute bottom-[50%] [transform:translate(0,50%)] flex flex-row items-center w-[100vw] px-[15px] lg:px-[55px] hover:cursor-pointer">
+        <button class="left-arrow pointer-events-auto w-[30px] h-[30px] lg:w-[80px] lg:h-[80px] hover:cursor-pointer" on:click={carouselLeft}>
             <img src="/assets/arrow-left.svg" alt="left arrow" />
         </button>
-        <button class="right-arrow ml-auto w-[30px] h-[30px] lg:w-[80px] lg:h-[80px] hover:cursor-pointer" on:click={carouselRight}>
+        <button class="right-arrow pointer-events-auto ml-auto w-[30px] h-[30px] lg:w-[80px] lg:h-[80px] hover:cursor-pointer" on:click={carouselRight}>
             <img src="/assets/arrow-right.svg" alt="right arrow" />
         </button>
     </div>
 </div>
 
-<div class="gallery-modal fixed top-[60px] lg:top-[100px] hidden w-[100vw] h-[calc(100vh-60px)] lg:h-[calc(100vh-100px)]" bind:this={modal}>
-    <button class="modal-mask w-full h-full" on:click={toggleGalleryModal}>
-        <button class="w-[20px] h-[20px] lg:w-[40px] lg:h-[40px]">
+<div class="gallery-modal fixed top-0 left-0 hidden w-[100vw] h-[100vh] z-20" bind:this={modal}>
+    <div class="relative">
+        <button class="modal-mask absolute w-[100vw] h-[100vh] top-0 left-0 bg-black opacity-70" on:click={toggleGalleryModal}>
+        </button>
+        <button class="absolute top-[20px] right-[20px] w-[20px] h-[20px] lg:w-[40px] lg:h-[40px] z-[21] opacity-100" on:click={toggleGalleryModal}>
             <img class="w-full h-full" src="/assets/close-button.svg" alt="close modal mask">
         </button>
-    </button>
-    <div class="image-wrapper" bind:this={modal_image}></div>
+        <div class="absolute top-0 left-0 z-[21] pointer-events-none hover:cursor-pointer">
+            <div class="w-[100vw] h-[100vh] flex flex-row items-center justify-center">
+                <div class="image-wrapper w-[90vw] lg:w-[75vw] h-[90vh]" bind:this={modal_image}></div>
+            </div>
+        </div>
+    </div>
 </div>
