40eff961a6110454819f7ea540d7a307713793e8
bengarrr <kevindwaite@gmail.com>
2023-08-28 17:50:12 -0700

    image optimizations

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -14,9 +14,8 @@
 <div class="image-wrapper {wrapperClasses ? wrapperClasses : null}">
     {#if blok.image}
         <img 
-            src="{blok.image.filename}/m/500x0 500w"
-            srcset="{blok.image.filename}/m/500x0 500w,
-            {blok.image.filename}/m/800x0 800w"
+            src="{blok.image.filename}/m/"
+            srcset="{blok.image.filename}/m/"
             sizes="100vw" 
             class={imgClasses} 
             alt={blok.image.description} 
